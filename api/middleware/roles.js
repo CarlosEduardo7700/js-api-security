3ed48c618c7b9bc5e990688a16f0d6a7f236c707
CarlosEduardo7700 <carlos.msilva@quintoandar.com.br>
2025-03-05 12:33:35 -0300

    feat: Criando o middleware que verifica as roles do usuário

---
+++ api/middleware/roles.js
@@ -0,0 +1,37 @@
+const { where } = require('sequelize')
+const database = require('../models')
+
+const roles = (listaRoles) => {
+    return async (req, res, next) => {
+        const { usuarioId } = req
+
+        const usuario = await database.usuarios.findOne({
+            include: [
+                {
+                    model: database.roles,
+                    as: 'usuario_das_roles',
+                    attributes: ['id', 'nome']
+                }
+            ],
+            where: {
+                id: usuarioId
+            }
+        })
+
+        if (!usuario) {
+            res.status(401).send('O usuário não foi encontrdo!')
+        }
+
+        const rolesCadastradas = usuario.usuario_das_roles
+            .map((role) => role.nome)
+            .some((role) => listaRoles.include(role))
+
+        if (!rolesCadastradas) {
+            res.status(401).send('O usuário não possui acesso a essa rota!')
+        }
+
+        return next()
+    }
+}
+
+module.exports - roles
