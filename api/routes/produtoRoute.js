1aa8118a09a5eb3ad208e0253626d54868c2674f
CarlosEduardo7700 <carlos.msilva@quintoandar.com.br>
2025-03-05 13:03:27 -0300

    test: Adicionando a validação de roles e permissões em uma rota

--- api/routes/produtoRoute.js
+++ api/routes/produtoRoute.js
@@ -9,7 +9,7 @@ router
   .post('/produto', ProdutoController.cadastrarProduto)
   .get('/produto', roles(["Vendedor", "Gerente"]), ProdutoController.buscarTodosProdutos)
   .get('/produto/id/:id', permissoes(["Listar por Id"]), ProdutoController.buscarProdutoPorId)
-  .delete('/produto/id/:id', ProdutoController.deletarProdutoPorId)
+  .delete('/produto/id/:id', roles(["Gerente", "Vendedor"]), permissoes(["excluir"]), ProdutoController.deletarProdutoPorId)
   .put('/produto/id/:id', ProdutoController.editarProduto)
 
 module.exports = router
